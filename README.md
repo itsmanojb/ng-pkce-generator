9957e9993b67ad3b16257c4f55afa32e11fd8665
itsmanojb <the_manoj@live.com>
2020-06-28 14:51:23 +0530

    :tada: Initial commit

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
-# NgPkceGenerator
+# Angular PKCE Generator
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.6.
 
+Proof Key for Code Exchange (PKCE) PKCE (pronounced "pixy") is a security extension to OAuth 2.0 for public clients on mobile devices, designed to prevent interception of the authorisation code by a malicious application that has sneaked into the same device.
+
+## Dependencies
+
+- [crypto-js](https://www.npmjs.com/package/crypto-js)
+
 ## Development server
 
+> 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+![ScreenShot](/login.png?raw=true "")
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
-
+import * as CryptoJS from 'crypto-js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-pkce-generator';
+
+  title = 'Angular PKCE';
+
+  codeVerifier = '';
+  codeChallange = '';
+
+  cvGenerated = false;
+  ccGenerated = false;
+  cvCopied = false;
+  ccCopied = false;
+
+  generateCodeVerifier() {
+    this.cvCopied = false;
+    this.codeVerifier = this.generateRandomString(128);
+    this.cvGenerated = true;
+    this.ccGenerated = false;
+    this.ccCopied = false;
+  }
+
+  generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+  generateCodeChallenge(cv: string) {
+    this.ccCopied = false;
+    this.ccGenerated = true;
+    const cc = this.base64URL(CryptoJS.SHA256(this.codeVerifier))
+    this.codeChallange = cc;
+  }
+
+  base64URL(string: any) {
+    return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+  }
+
+  copyToClipboard(field: string) {
+    const item = field === 'cv' ? this.codeVerifier : this.codeChallange;
+    if (field === 'cv') {
+      if (!this.cvGenerated) return;
+      this.cvCopied = true;
+    } else {
+      if (!this.ccGenerated) return;
+      this.ccCopied = true;
+    }
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.clipboardData.setData('text/plain', (item));
+      e.preventDefault();
+      document.removeEventListener('copy', null);
+    });
+    document.execCommand('copy');
+  }
 }
